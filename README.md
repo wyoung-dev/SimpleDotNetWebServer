a40d9689483a0165d123d25c859aaa1ff61380e9
Wayland Young <wyoung.dev@gmail.com>
2024-01-19 17:04:04 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demonstrates how to create a simple web server using the .NET implementation of Berkley sockets.
+
+To run the web server, build the project and execute 'ws.exe' (or 'ws') from the build directory. The IP address and port will default to http://127.0.0.1:80 unless specified as arguments, i.e., "ws {ip address} {port}".
---
+++ SimpleDotNetWebServer/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Serilog;
+
+// Demonstrates how to create a simple web server using the .NET implementation of Berkley sockets.
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Setup Serilog.
+        Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+        string ipAddress;
+        int port;
+
+        if (args.Length == 0)
+        {
+            ipAddress = "127.0.0.1";
+            port = 80;
+        }
+        else
+        {
+            ipAddress = args[0];
+            port = int.Parse(args[1]);
+        }
+
+        // Setup the listener/server socket using the IP address family, socket type 'stream' for reliable, two-way streaming, and 'tcp' protocol type.
+        using var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the given IP address and port.
+        serverSocket.Bind(new IPEndPoint(IPAddress.Parse(ipAddress), port));
+        serverSocket.Listen();
+
+        Log.Information("""
+                Listening at: {prefix}{endpoint}:{port}
+                Press Ctrl+C to exit.
+
+                """, "http://", ipAddress, port);
+
+        const int bufferSize = 1024;
+
+        // Enter a continuous loop to accept incoming connections.
+        while (true)
+        {
+            var _ = await serverSocket.AcceptAsync()
+                .ConfigureAwait(false);
+
+            // Start a new task for each client.
+            await Task.Factory.StartNew(async state =>
+            {
+                if (state is not Socket clientSocket) return;
+
+                // NOTE: Data must be received before a reply can be sent.
+                var bytesRead = 0;
+                var requestBuffer = new byte[bufferSize];
+                using var requestStream = new MemoryStream();
+
+                do
+                {
+                    bytesRead = await clientSocket.ReceiveAsync(requestBuffer.AsMemory(0, bufferSize))
+                        .ConfigureAwait(false);
+                    await requestStream.WriteAsync(requestBuffer.AsMemory(0, bytesRead))
+                        .ConfigureAwait(false);
+                }
+                while (bytesRead == bufferSize);
+
+                var request = Encoding.UTF8.GetString(requestStream.ToArray());
+
+                // Log the request.
+                Log.Debug("""
+
+            {request}
+            """, request);
+
+                // Send the reply.
+                // NOTE: New line is expected after the HTTP response code and the response payload.
+                await clientSocket.SendAsync(Encoding.UTF8.GetBytes("""
+                HTTP/1.1 200 OK
+
+                Hello World!
+                """)).ConfigureAwait(false);
+
+                // It's important to close and dispose of the incoming socket.
+                clientSocket.Close();
+                clientSocket.Dispose();
+            }, _);
+        }
+    }
+}
